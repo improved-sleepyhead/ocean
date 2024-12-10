af832dd5304d7e099c7d33c12e7d33ea740aec03
MRDick <you@example.com>
2024-12-10 10:46:40 +0300

    modified:   app/(marketing)/_components/navbar.tsx 	new file:   components/spinner.tsx

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,13 +2,17 @@
 
 import { useScrollTop } from "@/hooks/use-scroll-top"
 import { ModeToggle } from "@/components/mode-toggle";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
 import { useConvexAuth } from "convex/react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
 
 
 export const Navbar = () =>{
-    const {} = useConvexAuth();
+    const { isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
     return(
         <div className={cn(
@@ -17,6 +21,34 @@ export const Navbar = () =>{
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading &&(
+                <>
+                    <SignInButton mode="modal">
+                        <Button variant="ghost" size="sm" className="text-[#0059FF] hover:text-[#0059FF] dark:text-[#ffffff] dark:hover:text-[#cccccc]">
+                            Войти
+                        </Button>
+                    </SignInButton>
+                    <SignInButton mode="modal">
+                        <Button size="sm" className="bg-[#0044C2] px-8 hover:bg-[#19489d] dark:bg-white dark:hover:bg-[#cccccc]">
+                            Знакомство с Ocean
+                        </Button>
+                    </SignInButton>
+                </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <Button className="text-[#0059FF] hover:text-[#0059FF] dark:text-[#ffffff] dark:hover:text-[#cccccc]" 
+                        variant="ghost" size="sm"  asChild>
+                            <Link href="/documents">
+                            Перейти в Ocean
+                            </Link>
+                        </Button>
+                        <UserButton />
+                    </>
+                )}
                 <ModeToggle />
             </div>
         </div> 
