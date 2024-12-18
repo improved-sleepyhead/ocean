fdd5bc291c6943dd38ca9e55dc11d3e58fda40c7
MRDick <you@example.com>
2024-12-18 15:37:42 +0300

    new file:   app/(main)/_components/user-item.tsx 	deleted:    public/assets/Montserrat-ExtraBold.ttf 	new file:   public/default.svg 	new file:   public/default_dark.svg 	deleted:    public/logo_dark.png

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
 const DocumentsPage = () => {
+    const { user } = useUser();
+
     return ( 
-        <div>
-            This is Documents page!
+        <div className="h-full flex flex-col items-center justify-center space-y-5">
+            <Image
+                src="/default.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="dark:hidden"
+            />
+            <Image
+                src="/default_dark.svg"
+                height="300"
+                width="300"
+                alt="Empty"
+                className="hidden dark:block"
+            />
+            <h1 className="text-lg font-montserrat font-medium">
+                {user?.firstName}, добро пожаловать в твой Ocean!
+            </h1>
+            <Button>
+                <PlusCircle className="h-4 w-4 mr-2"/>
+                Создать заметку
+            </Button>
         </div>
     );
 }
