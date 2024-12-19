281c7d4962e42b5b87f123c23f06c99535a98197
MRDick <you@example.com>
2024-12-19 11:10:49 +0300

    modified:   app/(main)/_components/navigation.tsx 	modified:   app/(main)/_components/user-item.tsx 	new file:   components/ui/avatar.tsx 	modified:   components/ui/dropdown-menu.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronLeft, MenuIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ComponentRef, useEffect, useRef, useState } from "react";
 import {useMediaQuery} from "usehooks-ts";
+import { UserItem } from "./user-item";
 
 export const Navigation = () => {
     const pathname = usePathname();
@@ -110,7 +111,7 @@ export const Navigation = () => {
                     <ChevronLeft className="h-6 w-6"/>
                 </div>
                 <div>
-                    <p>Action items</p>
+                    <UserItem/>
                 </div>
                 <div className="mt-4">
                     <p>Documents</p>
---
+++ app/(main)/_components/user-item.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { ChevronsLeftRight } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+
+import {
+    Avatar,
+    AvatarImage
+} from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+export const UserItem = () =>{
+    const {user} = useUser();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div role="button" className="flex items-center text-sm p-3
+                w-full hover:bg-primary/5">
+                    <div className="gap-x-2 flex items-center max-w-[150px]">
+                        <Avatar className="h-5 w-5">
+                            <AvatarImage src={user?.imageUrl}/>
+                        </Avatar>
+                        <span className="text-start font-medium line-clamp-1">
+                            {user?.fullName}&apos;s Ocean
+                        </span>
+                    </div>
+                </div>
+            </DropdownMenuTrigger>
+        </DropdownMenu>
+    )
+}
